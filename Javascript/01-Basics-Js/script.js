2a27d9db6a2266949eb4851e66afdfa17f4fc095
Om Anandani <omanandani@gmail.com>
2026-08-25 21:06:23 +0530

    Day 1-2: Added Javascript learning topics

---
+++ Javascript/01-Basics-Js/script.js
@@ -0,0 +1,27 @@
+// let count = 8;
+
+// console.log("count" + " = " + count);
+// console.log("count" + " = " + `${count}`);
+
+// const myAge = 24;
+// console.log(`${myAge}`);
+
+// let myAge = 24;
+// let humandogratio = 2;
+
+// let myDogAge = myAge * humandogratio;
+// console.log(myDogAge)
+
+// let bonusPoints = 50;
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+
+function increment(){
+    console.log("the button was clicked");
+}
---
+++ Javascript/02-Functions-JS/script.js
@@ -0,0 +1,39 @@
+// Setting up the race
+// function countDown(){
+// console.log(5);
+// console.log(4);
+// console.log(3);
+// console.log(2);
+// console.log(1);
+// }
+
+// countDown();
+
+// GO
+// Players are running in the race
+// Race is Finished!
+
+// Get ready for a new race.
+// countDown();
+
+// assignment 1 -- print 42 while calling a function name whatever you want the function
+
+// function called(){
+//     console.log(42);
+// }
+
+// called();
+
+// assignement 2 -- create a function that logs the sum of all the lap times.
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+let sum = 0;
+
+function sumOfLaps(){
+    sum = lap1+lap2+lap3;
+    console.log(sum);   
+}
+
+sumOfLaps();
