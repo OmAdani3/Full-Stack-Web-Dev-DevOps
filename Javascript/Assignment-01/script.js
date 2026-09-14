44e1de5883fe81940c2c90532427d20d3152e027
Om Anandani <omanandani@gmail.com>
2026-09-14 19:29:08 +0530

    Did some assignments today

---
+++ Javascript/Assignment-01/script.js
@@ -0,0 +1,85 @@
+// Assignment - 01
+
+// Create two variables FirstName and LastName
+// let firstName = "Tech";
+// let lastName = "Gamer";
+
+// Concatenate two variables into a third variable called fullName
+// let fullName = firstName + " " + lastName;
+
+
+// log fullName to the console.
+
+// console.log(fullName);
+
+
+// ------- Assignment - 02
+
+// let name = "Linda";
+// let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+// function greet(){
+//     let greeTing = greeting +", "+ name +"!";
+//     console.log(greeTing);
+// }
+
+// greet();
+
+
+// ------------- Assignment - 03
+// let myPoints = 3;
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// Add/Remove points to/from the mypoints variable.
+
+// function add3Points(){
+//     myPoints +=3;
+// }
+
+// function remove1Point(){
+//     myPoints--;
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+
+// remove1Point();
+// remove1Point();
+
+// call the functions so that the line below logs out 10.
+// console.log(myPoints);
+
+
+// --------------------Assignment - 04
+
+// Try to predict what each of the lines will log out.
+
+// console.log("2" + 2);    //22
+// console.log(11+7);       // 18
+// console.log(6 + "5");    //65
+// console.log("My Points " + 5 + 9);     // My Points 14
+// console.log(2+2)        //4
+// console.log("11" + 14);         // 1114
+
+
+
+// ------------------Assignment - 05
+
+// when the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragrapgh
+// that has the id="error";
+
+
+// let err = document.getElementById("error") 
+
+// function purchase(){
+//     error.textContent = "Something went wrong, please try again";
+// }
+
+
+
+
+// --------------------------ASIGNMENT - 06
